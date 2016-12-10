36e87b5ab6f9a41d98d31a6826973b349edad30d
tsotnesharvadze <tsotnesharvadze@gmail.com>
2016-12-10 19:34:08 +0400

    pagination_part_1

--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post 
 from .forms import CommentForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 # Create your views here.
 def index(request):
@@ -13,8 +15,20 @@ def index(request):
 	popularuli_postebi.sort(key=my_sort,reverse=True)
 	popularuli_postebi=popularuli_postebi[:5]
 
+	paginator = Paginator(postebis_sia, 2) # Show 25 contacts per page
+
+	page = request.GET.get('page')
+	try:
+		contacts = paginator.page(page) 
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		contacts = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		contacts = paginator.page(paginator.num_pages)
+
 	return render(request,'app/new.html',{"postebis_sia":postebis_sia,
-		"popularuli_postebi":popularuli_postebi})
+		"popularuli_postebi":popularuli_postebi,"contacts":contacts})
 
 
 def srulad(request,posti_id):
