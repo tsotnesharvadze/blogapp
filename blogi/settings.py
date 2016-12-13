1027dcafbd2e4c3e74edfa45d730974633064753
tsotnesharvadze <tsotnesharvadze@gmail.com>
2016-12-13 18:22:02 +0400

    shadgusa

--- blogi/settings.py
+++ blogi/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 from django.utils.translation import ugettext_lazy as _
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ka'
 
 TIME_ZONE = 'UTC'
 
