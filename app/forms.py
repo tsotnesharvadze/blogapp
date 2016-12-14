625f34a8f8a3da4f22b7d164b465e3c44bf5b616
tsotnesharvadze <tsotnesharvadze@gmail.com>
2016-12-14 22:44:59 +0400

    form not save

--- app/forms.py
+++ app/forms.py
@@ -2,15 +2,10 @@ from django import forms
 from .models import Comment
 from django.utils.translation import ugettext_lazy as _
 class CommentForm(forms.Form):
-<<<<<<< HEAD
 	class Meta:
 		model = Comment
 		fields = ['course']
 	new_comment = forms.CharField(label='კომენტარი ', max_length=100)
-=======
-    new_comment = forms.CharField(label=_('კომენტარი '), max_length=100)
-    class Meta:
->>>>>>> 81a5fbffaf0ec4f216d57a66d547c4dc56ebc95a
 
 	def clean_email(self):
 		comment=self.cleaned_data.get("kometari")
